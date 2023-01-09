d22287910873b365e54c246a871610c10db7ca42
Bz <92101931+smxl@users.noreply.github.com>
2023-01-09 19:53:12 +0800

    tiny fixed

--- workers.kv.js
+++ workers.kv.js
@@ -17,8 +17,7 @@ async function handleRequest(request) {
     } else {
       return new Response(JSON.stringify({
         status: 404,
-        message: 'Key not found',
-        urls: null
+        message: 'Key not exist'
       }), {
         status: 404,
         statusText: 'Not found',
@@ -34,11 +33,11 @@ async function handleRequest(request) {
       while (await YuhURLs.get(newkey)) {
         return new Response(JSON.stringify({
         status: 400,
-        message: 'Duplicate',
+        message: 'Occupied',
         newkey
       }), {
         status: 400,
-        statusText: 'Not found',
+        statusText: 'Bad Request',
         headers: { "Content-Type": "application/json" }
       });
       }
@@ -68,7 +67,9 @@ async function handleRequest(request) {
   let html_style = `body,html{width:100%;height:100%}
 body{padding:0; margin:0 !important}
 body {background: linear-gradient(to bottom, #000 0%, #233 100%)}
-#container {display: flex;flex-direction:column;align-items: center;justify-content: center;height: 100%;color:white;font-family:sans-serif}`;
+#container {display: flex;flex-direction:column;align-items: center;justify-content: center;height: 100%;color:white;font-family:sans-serif}
+.link {color:#fff;font-family:monospace;padding-right:25px}
+.codeblock {font-family:monospace;color:#000;background-color:#e2e2e2;padding:2px}`;
 
   html_content += `<h1>URL Shortener</h1>
     <h2>Pre</h2>
@@ -82,8 +83,8 @@ body {background: linear-gradient(to bottom, #000 0%, #233 100%)}
     6. Set Triggers - Custom Domains or Routers</br>
     Tips: you can change YuhURLs as what you want, remember replace in code</p>
     <h3>Usage</h3>
-    <p>1. $ curl ${protocol}//${hostname}/?key=yuh&url=http://example.com</br>
-    2. Open in Browser <a href="${protocol}//${hostname}/?key=yuh&url=http://example.com">${protocol}//${hostname}/?key=yuh&url=http://example.com</a></p>
+    <p>1. <code class="codeblock">$ curl ${protocol}//${hostname}/?key=yuh&url=http://example.com</code></br>
+    2. Open in Browser <a class="link" href="${protocol}//${hostname}/?key=yuh&url=http://example.com">${protocol}//${hostname}/?key=yuh&url=http://example.com</a></p>
     <h4>Test</h4>
     <pre>${protocol}//${hostname}/yuh</pre>
   `;
