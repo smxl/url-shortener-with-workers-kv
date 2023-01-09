b967594c165b6c73d5540cca0e00085a96ffff59
Bz <92101931+smxl@users.noreply.github.com>
2023-01-09 20:05:10 +0800

    Update workers.kv.js

--- workers.kv.js
+++ workers.kv.js
@@ -33,7 +33,7 @@ async function handleRequest(request) {
       while (await YuhURLs.get(newkey)) {
         return new Response(JSON.stringify({
         status: 400,
-        message: 'Occupied',
+        message: 'Key unavailable',
         newkey
       }), {
         status: 400,
@@ -66,7 +66,7 @@ async function handleRequest(request) {
   let html_content = "";
   let html_style = `body,html{width:100%;height:100%}
 body{padding:0; margin:0 !important}
-body {background: linear-gradient(to bottom, #000 0%, #233 100%)}
+body{background:linear-gradient(to bottom, #111 30%, #222 90%)}
 #container {display: flex;flex-direction:column;align-items: center;justify-content: center;height: 100%;color:white;font-family:sans-serif}
 .link {color:#fff;font-family:monospace;padding-right:25px}
 .codeblock {font-family:monospace;color:#000;background-color:#e2e2e2;padding:2px}`;
@@ -83,7 +83,7 @@ body {background: linear-gradient(to bottom, #000 0%, #233 100%)}
     6. Set Triggers - Custom Domains or Routers</br>
     Tips: you can change YuhURLs as what you want, remember replace in code</p>
     <h3>Usage</h3>
-    <p>1. <code class="codeblock">$ curl ${protocol}//${hostname}/?key=yuh&url=http://example.com</code></br>
+    <p>1. <code class="codeblock">$ curl '${protocol}//${hostname}/?key=yuh&url=http://example.com'</code></br>
     2. Open in Browser <a class="link" href="${protocol}//${hostname}/?key=yuh&url=http://example.com">${protocol}//${hostname}/?key=yuh&url=http://example.com</a></p>
     <h4>Test</h4>
     <pre>${protocol}//${hostname}/yuh</pre>
